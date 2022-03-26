1cc1b6d49c5f0b3df5e6778c9c596266e24bea11
Andrei Huk <belakolala1@gmail.com>
2022-03-27 02:50:02 +0300

    Добавьте файлы проекта.

---
+++ Task_10_Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Type;
+
+namespace Task_10_Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            var myList = new MyList();
+            var item1 = new MyItem() { Name = "test1", Age = random.Next(0,100) };
+            var item2 = new MyItem() { Name = "test2", Age = random.Next(0, 100) };
+            var item3 = new MyItem() { Name = "test3", Age = random.Next(0, 100) };
+            var item4 = new MyItem() { Name = "test4", Age = random.Next(0, 100) };
+            var item5 = new MyItem() { Name = "test5", Age = random.Next(0, 100) };
+            var item6 = new MyItem() { Name = "test6", Age = random.Next(0, 100) };
+            var item7 = new MyItem();
+
+            myList.Add(item1);
+            myList.Add(item2);
+            myList.Add(item3);
+            myList.Add(item4);
+            myList.Add(item5);
+            myList.Add(item6);
+            myList.Add(item7);
+
+            foreach (MyItem item in myList)
+            {
+                Console.WriteLine($"{item.Name}\t{item.Age}");
+            }
+        }
+    }
+}
---
+++ Type/Types.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+
+namespace Type
+{
+    public class MyList : IList<MyItem>
+    {
+        MyItem[] myItems;
+        public MyList()
+        {
+            myItems = new MyItem[0];
+        }
+        public MyItem this[int index] { get { return myItems[index]; } set { myItems[index] = value; } }
+        public int Count { get; private set; }
+        public bool IsReadOnly { get { return false; } }
+        public void Add(MyItem item)
+        {
+            MyItem[] newMyItem = new MyItem[myItems.Length+1];
+            for (int i = 0; i < myItems.Length; i++)
+            {
+                newMyItem[i] = myItems[i];
+            }
+            newMyItem[myItems.Length] = item;
+            myItems = newMyItem;
+        }
+        public void Clear()
+        {
+            Count = 0;
+        }
+        public bool Contains(MyItem item)
+        {
+            for (int i = 0; i < myItems.Length; i++)
+            {
+                if (myItems[i] == item)
+                    return true;
+            }
+            return false;
+        }
+        public void CopyTo(MyItem[] array, int arrayIndex)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                array.SetValue(myItems[i], arrayIndex++);
+            }
+        }
+        public IEnumerator<MyItem> GetEnumerator()
+        {
+            foreach (MyItem item in this.myItems)
+            {
+                yield return item;
+            }
+        }
+        public int IndexOf(MyItem item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (myItems[i] == item)
+                    return i;
+            }
+            return -1;
+        }
+        public void Insert(int index, MyItem item)
+        {
+            if ((Count+1 <= myItems.Length) && (index < Count) && (index >=0))
+            {
+                Count++;
+                for (int i = Count - 1; i > index; i--)
+                {
+                    myItems[i] = myItems[i - 1];
+                }
+                myItems[index] = item;
+            }
+        }
+        public bool Remove(MyItem item)
+        {
+            if (myItems.Contains(item))
+                return true;
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+            if ((index >=0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    myItems[i] = myItems[i + 1];
+                }
+                Count--;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+    public class MyItem : IEnumerator
+    {
+        private string _Name = "Unknow";
+        public string Name
+        {
+            set
+            {
+                _Name = value == null ? value = _Name : _Name = value;
+            }
+            get { return _Name; }
+        }
+        public int Age { get; set; }
+        readonly MyList[] myList;
+        int position = -1;
+        private MyList Current 
+        {
+            get
+            {
+                try
+                {
+                    return myList[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        object IEnumerator.Current { get { return Current; } }
+        public bool MoveNext()
+        {
+            position++;
+            return (position < myList.Length);
+        }
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
